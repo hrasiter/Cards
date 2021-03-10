da2d2ad8c2054a56011d816fc63f6778770d7e90
Rasite <hrasiter@gmail.com>
2021-03-11 00:49:09 +0300

    Declaring variable and func

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi there!")
+	/*Declaring long way varible*/
+	//var card string = "Ace of Space"
+
+	/*Declaring short hand variable*/
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
